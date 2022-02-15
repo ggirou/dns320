e73a0fa80f9b723b712814617ac7fb8e26e1fd81
Guillaume Girou <git@modoki.ovh>
2022-02-15 19:05:26 +0100

    Upgrade debootstrap to bullseye. Add dbus-user-session needed by dconf-service. Refactor deboot.sh

--- scripts/deboot.sh
+++ scripts/deboot.sh
@@ -1,18 +1,85 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
+
+# https://www.debian.org/releases/bullseye/armel/apds03.en.html
 
 arch=$1
 suite=$2
 mirror=$3
 
-include="linux-image-marvell,device-tree-compiler,openssh-server,sudo,locales,tzdata,ntpdate,console-data,gnupg,apt-transport-https,ca-certificates,curl,nano,udev,dialog,ifupdown,mtd-utils,procps,iputils-ping,isc-dhcp-client,wget,netbase,net-tools,acl,openssh-server,avahi-daemon,python-minimal,u-boot-tools,hdparm,samba"
 hostname=DNS-320
+
+# https://wiki.debian.org/BusterPriorityRequalification
+# --variant=minbase
+# dpkg-query -f '${binary:Package} ${Priority}\n' -W | grep -w 'required\|important'
+
+packages=(
+  # Kernel / DNS-320
+  linux-image-marvell # Linux for Marvell Kirkwood/Orion (meta-package)
+  device-tree-compiler # Device Tree Compiler for Flat Device Trees
+
+  # Admin
+  dbus-user-session # simple interprocess messaging system (systemd --user integration)
+  hdparm # tune hard disk parameters for high performance
+  mtd-utils # Memory Technology Device Utilities
+  procps # /proc file system utilities
+  u-boot-tools # companion tools for Das U-Boot bootloader
+  udev # /dev/ and hotplug management daemon
+
+  # Localization
+  locales # GNU C Library: National Language (locale) data [support]
+  tzdata # time zone and daylight-saving time data
+
+  # Misc
+  ca-certificates # Common CA certificates
+
+  # Network
+  avahi-daemon # Avahi mDNS/DNS-SD daemon
+  ifupdown # high level tools to configure network interfaces
+  iputils-ping # Tools to test the reachability of network hosts
+  isc-dhcp-client # DHCP client for automatically obtaining an IP address
+  net-tools # NET-3 networking toolkit
+  netbase # Basic TCP/IP networking system
+  # ntpdate # client for setting system time from NTP servers (deprecated)
+  sntp # Network Time Protocol - sntp client
+
+  # Network - Servers
+  netatalk # Basic TCP/IP networking system
+  nfs-kernel-server # support for NFS kernel server
+  openssh-server # secure shell (SSH) server, for secure access from remote machines
+  samba # SMB/CIFS file, print, and login server for Unix
+
+  # Python
+  python3-minimal # minimal subset of the Python language (default python3 version)
+
+  # Utils
+  acl # access control list - utilities
+  console-data # keymaps, fonts, charset maps, fallback tables for 'kbd'.
+  curl # command line tool for transferring data with URL syntax
+  dialog # Displays user-friendly dialog boxes from shell scripts
+  gnupg # GNU privacy guard - a free PGP replacement
+  nano # small, friendly text editor inspired by Pico
+  sudo # Provide limited super user privileges to specific users
+  wget # retrieves files from the web
+)
 
 mkdir -p /chroot
 
-[ -f /dist/$suite-$arch.tar ] \
-  && tar xf /dist/$suite-$arch.tar -C /chroot \
-  || (qemu-debootstrap --arch=$arch --include=$include $suite /chroot $mirror \
-      && tar cf /dist/$suite-$arch.tar -C /chroot/ .)
+if [ -f /dist/$suite-$arch.tar ]; then
+    echo 'Untar existing deboostrap'
+    tar xf /dist/$suite-$arch.tar -C /chroot
+else
+    echo 'Run deboostrap'
+
+    function join { local IFS="$1"; shift; echo "$*"; }
+    include=$(join , ${packages[@]})
+    echo $include
+
+    # apt update && apt-cache search "^($(join \| ${packages[@]}))$"
+
+    debootstrap --arch=$arch --variant=minbase --include=$include $suite /chroot $mirror \
+       || (cp /chroot/debootstrap/debootstrap.log /dist && exit 1)
+    tar cf /dist/$suite-$arch.tar -C /chroot/ .
+fi
 
 ls -la /chroot
 
