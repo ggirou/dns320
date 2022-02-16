7e6dbe2392396a6182967bcf19524d0369511c58
Guillaume Girou <ggirou@users.noreply.github.com>
2022-02-16 23:13:13 +0100

    Update build_uboot.sh

--- scripts/build_uboot.sh
+++ scripts/build_uboot.sh
@@ -13,7 +13,13 @@ fi
 
 git config user.email "john@doe.xyz"; git config user.name "John Doe"
 
-# git checkout v2020.04; git merge --squash v2020.04-dns320; git format-patch HEAD~1
+# Commands to recreate patch for a new version
+# oldVersion=v2020.04 newVersion=v2022.01 patch=usbtimeoutfix
+# oldVersion=v2020.04 newVersion=v2022.01 patch=dns320
+# git checkout tags/$oldVersion; git am --committer-date-is-author-date < ~/$oldVersion-$patch.patch; git checkout -b $oldVersion-$patch
+# git checkout tags/$newVersion; git merge --squash $oldVersion-$patch; # Resolve conflicts...
+# git commit -m "$newVersion-$patch"; git format-patch --stdout HEAD~1 > ~/$newVersion-$patch.patch
+
 # Apply `EHCI timed out on TD` patch from https://forum.doozan.com/read.php\?3,35295
 git am --committer-date-is-author-date < /scripts/v2020.04-usbtimeoutfix.patch
 # Apply DNS-320 support patch from https://github.com/avoidik/board_dns320
