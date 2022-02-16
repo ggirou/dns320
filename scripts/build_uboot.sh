7a3ea070e81ab8f8962c5c6d96eb0186efc69580
Guillaume Girou <guillaume.girou@it-ce.fr>
2022-02-16 23:13:47 +0100

    Report changes from DNS-325

--- scripts/build_uboot.sh
+++ scripts/build_uboot.sh
@@ -13,11 +13,14 @@ fi
 
 git config user.email "john@doe.xyz"; git config user.name "John Doe"
 
+# Check what changes with similar DNS-325
+# git diff tags/$oldVersion tags/$newVersion -- arch/arm/dts/kirkwood-dns* board/d-link/dns325/* configs/dns325* include/configs/dns325*
+
 # Commands to recreate patch for a new version
 # oldVersion=v2020.04 newVersion=v2022.01 patch=usbtimeoutfix
 # oldVersion=v2020.04 newVersion=v2022.01 patch=dns320
-# git checkout tags/$oldVersion; git am --committer-date-is-author-date < ~/$oldVersion-$patch.patch; git checkout -b $oldVersion-$patch
-# git checkout tags/$newVersion; git merge --squash $oldVersion-$patch; # Resolve conflicts...
+# git checkout tags/$oldVersion; git checkout -b $oldVersion-$patch; git am --committer-date-is-author-date < ~/$oldVersion-$patch.patch
+# git checkout tags/$newVersion; git merge --squash $oldVersion-$patch; # Resolve conflicts and report changes from DNS-325......
 # git commit -m "$newVersion-$patch"; git format-patch --stdout HEAD~1 > ~/$newVersion-$patch.patch
 
 # Apply `EHCI timed out on TD` patch from https://forum.doozan.com/read.php\?3,35295
